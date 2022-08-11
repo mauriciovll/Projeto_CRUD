06e640dbf18ad3b9a196c5eaf8bb1535f53ffab2
Mauricio Vasconcellos Leão Lyrio <mauriciovll@gmail.com>
2022-08-11 18:47:22 -0300

    Inclusão de alertas
    Incluídos os scripts de alerta no arquivo de rotas e na view de cadastro de clientes.

--- routes/index.js
+++ routes/index.js
@@ -4,45 +4,69 @@ const db = require('../db');
 
 /* GET home page. */
 router.get('/', async function(req, res) {
-  res.render('index', { docs: await db.findAll()});
+  try{
+    res.render('index', { docs: await db.findAll()});
+  }catch(ex){
+    res.redirect('/erro=${ex}');
+  }
 });
 
 /* GET new page. */
 router.get('/new', function(req, res, next){
-  res.render('new',{title:"Cadastro de Cliente", doc:{}, action:'/new'});
+  try{
+    res.render('new',{title:"Cadastro de Cliente", doc:{}, action:'/new'});
+  }catch(ex){
+    res.redirect('/erro=${ex}');
+  }
 });
 
 /* POST new page. */
 router.post('/new', async function(req, res){
-  const nome = req.body.nome;
-  const idade = parseInt(req.body.idade);
-  const uf = req.body.uf;
-  await db.insert({nome, idade, uf});
-  res.redirect('/?new=true');
+  try{
+    const nome = req.body.nome;
+    const idade = parseInt(req.body.idade);
+    const uf = req.body.uf;
+    await db.insert({nome, idade, uf});
+    res.redirect('/?new=true');
+  }catch{
+    res.redirect('/erro=${ex}');
+  }
 });
 
 /* GET edit page. */
 router.get('/edit/:id', async function(req, res, next){
-  const id = req.params.id;
-  const doc = await db.findOne(id);
-  res.render('new', {title:"Edição de Cliente", doc, action:'/edit/' + doc._id});
+  try{
+    const id = req.params.id;
+    const doc = await db.findOne(id);
+    res.render('new', {title:"Edição de Cliente", doc, action:'/edit/' + doc._id});
+  }catch{
+    res.redirect('/erro=${ex}');
+  }
 });
 
 /* POST edit page. */
 router.post('/edit/:id', async function(req, res){
-  const id = req.params.id;
-  const nome = req.body.nome;
-  const idade = parseInt(req.body.idade);
-  const uf = req.body.uf;
-  await db.update(id, {nome, idade, uf});
-  res.redirect('/?edit=true');
+  try{
+    const id = req.params.id;
+    const nome = req.body.nome;
+    const idade = parseInt(req.body.idade);
+    const uf = req.body.uf;
+    await db.update(id, {nome, idade, uf});
+    res.redirect('/?edit=true');
+  }catch{
+    res.redirect('/erro=${ex}');
+  }
 });
 
 /* GET delete page. */
 router.get('/delete/:id', async function(req, res){
-  const id = req.params.id;
-  await db.deleteOne(id);
-  res.redirect('/?delete=true');
+  try{
+    const id = req.params.id;
+    await db.deleteOne(id);
+    res.redirect('/?delete=true');
+  }catch{
+    res.redirect('/erro=${ex}');
+  }
 });
 
 module.exports = router;
